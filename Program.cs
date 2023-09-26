130d8eae65b3dd5b5c3470437fde4abfacb906d9
avon8080 <evanjlalor@gmail,com>
2023-09-26 11:39:16 -0500

    inclass Assignment

--- Program.cs
+++ Program.cs
@@ -10,11 +10,16 @@ namespace Class05
 
             IArmor armor = new Leather("Elven Leather Armor", 12);
             IWeapon sword = new Sword("Long sword", 5);
+            IArmor blackArmor = new BlackArmor("Black Steel Armor", 50);
+            IWeapon axe = new Axe("Black Steel Axe", 8);
+
+            ICombatant blackKnight = new Fighter("Black Knight", blackArmor, axe);
 
             ICombatant fighter = new Fighter("Fighter Fred", armor, sword);
             ICombatant badGuy = new Fighter("Scary Monster", armor, sword);
 
             fighter.Attack(badGuy);
+            blackKnight.Attack(fighter);
 
         }
 
